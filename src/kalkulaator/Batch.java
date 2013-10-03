81b7cb2b459735c4f6c3e27e19b0e81ea1159f12
Andreas Ots <andreasots@gmail.com>
2013-10-03 22:24:02 +0300

    Batch: file handling and a help message

--- src/kalkulaator/Batch.java
+++ src/kalkulaator/Batch.java
@@ -1,11 +1,27 @@
 package kalkulaator;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.InputStreamReader;
 
 public class Batch {
 	public static void main(String[] args) {
 		Parser parser = new Parser();
-	    BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	    BufferedReader input = null;
+	    if (args.length == 0) {
+	    	input = new BufferedReader(new InputStreamReader(System.in));
+	    } else if (args.length == 1) {
+			try {
+				input = new BufferedReader(new FileReader(new File(args[0])));
+			} catch (FileNotFoundException e1) {
+				System.err.printf("%s: File not found\n", args[0]);
+				return;
+			}
+		} else {
+	    	System.err.printf("Usage: %s [script]\n", Batch.class.getName());
+	    	return;
+		}
 	    
 		while (true) {
 			String expr = null;
